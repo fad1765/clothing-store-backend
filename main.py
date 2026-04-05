c9131fd12c7cd4291e79d9df8fdbc5938e42965b
fad1765 <qaq70326@gmail.com>
2026-04-05 23:18:37 +0800

    Update:測試錯誤

--- main.py
+++ main.py
@@ -1,46 +1,7 @@
-import os
-
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from routers import (
-    products,
-    users,
-    cart,
-    orders,
-    admin_users,
-    dashboard,
-    comments,
-    wishlist,
-    coupons,
-    admin_coupons,
-)
 
 app = FastAPI()
 
-if not os.path.exists("uploads"):
-    os.makedirs("uploads")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://my-react-two-zeta.vercel.app",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
-
-app.include_router(products.router)
-app.include_router(users.router)
-app.include_router(cart.router)
-app.include_router(orders.router)
-app.include_router(admin_users.router)
-app.include_router(dashboard.router)
-app.include_router(comments.router)
-app.include_router(wishlist.router)
-app.include_router(coupons.router)
-app.include_router(admin_coupons.router)
+@app.get("/")
+def root():
+    return {"message": "ok"}
