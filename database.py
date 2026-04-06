5b0ca65af134ea7dbf6d58b0753e7364c61afd37
fad1765 <qaq70326@gmail.com>
2026-04-06 13:46:40 +0800

    Update:修改檔案內容

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def get_connection():
     database_url = os.getenv("DATABASE_URL")
 
     if database_url:
-        return psycopg2.connect(database_url)
+        return psycopg2.connect(database_url, sslmode="require")
 
     return psycopg2.connect(
         host="localhost",
